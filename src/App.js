2e99572c1fe26944add4a6048b8f6fcd52890836
vladislavfva <vladislavfva@icloud.com>
2024-03-05 15:00:09 +0100

    Setup Project Changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,5 @@
-import Main from './components/Main/Main';
-
 function App() {
-  return (
-    <div className="App">
-      <Main />
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
