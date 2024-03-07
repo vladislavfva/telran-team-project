e9e3ee47de133edfd419ac79155f0a81a55b056f
vladislavfva <vladislavfva@icloud.com>
2024-03-07 11:20:59 +0100

    Change Footer Tag

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,7 @@
 import classes from './Footer.module.css'
 
 function Footer() {
-  return (<div className='container'>
+  return (<footer className='container'>
   <h2>Contact</h2>
   <address>
     <div>
@@ -24,7 +24,7 @@ function Footer() {
   <div className={classes.map}>
     <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2428.410460816817!2d13.372521313046862!3d52.507910471941436!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47a851cbdd6cfe0f%3A0xb4b0903f299decf1!2sLinkstra%C3%9Fe%202%2F8.%20Etage%2C%2010785%20Berlin!5e0!3m2!1sen!2sde!4v1709758192276!5m2!1sen!2sde" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
   </div>
-  </div>);
+  </footer>);
 }
 
 export default Footer;
