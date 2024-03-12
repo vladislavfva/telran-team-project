63cce6e76eee1c8bedcfd4fdaa610b7ffe738b12
Mekhova Kateryna <katrindeutschland33333@gmail.com>
2024-03-12 15:38:56 +0100

    fixed burger menu (add when pressed burger menu slides out), media

--- personal-docs/katia/README.md
+++ personal-docs/katia/README.md
@@ -1,10 +1,1 @@
-start work
-hello
 
-11.03 
-{burgerMenuOpen && ()} fix because the menu disappears
-
-
-I dont like:
-Jumping letters on the banner when the screen is reduced (zmenshuvaty)
-The image (banner) moves to much when the screen is reduced
--- src/components/HeaderNav/HeaderNav.jsx
+++ src/components/HeaderNav/HeaderNav.jsx
@@ -10,7 +10,11 @@ import { NavLink } from "react-router-dom";
 export const HeaderNav = () => {
   const [burgerMenuOpen, setBurgerMenuOpen] = useState(false);
 
-  
+ const handleClickToggle = () => {
+   setBurgerMenuOpen(!burgerMenuOpen);
+ };
+
+
   return (
     <div className={classes.container}>
       <div className={classes.logo_container}>
@@ -19,51 +23,53 @@ export const HeaderNav = () => {
           <SvgModeMoon />
         </div>
       </div>
-      {burgerMenuOpen && (
-        <div className={classes.central_container__nav}>
-          <button className={classes.btn}>1 day discount!</button>
-          <button
-            onClick={() => setBurgerMenuOpen(!burgerMenuOpen)}
-            className={classes.cross_btn}
-          >
-            <div className={classes.cross}>
-              <span className={classes.span_bevelled__top}></span>
-              <span className={classes.span_bevelled__botton}></span>
-            </div>
-          </button>
-          <nav className={classes.nav_container}>
-            <ul>
-              <li>
-                <NavLink className={classes.nav_element__style}>
-                  Main Page
-                </NavLink>
-              </li>
-            </ul>
-            <ul>
-              <li>
-                <NavLink className={classes.nav_element__style}>
-                  Categories
-                </NavLink>
-              </li>
-            </ul>
-            <ul>
-              <li>
-                <NavLink className={classes.nav_element__style}>
-                  All products
-                </NavLink>
-              </li>
-            </ul>
-            <ul>
-              <li>
-                <NavLink className={classes.nav_element__style}>
-                  All sales
-                </NavLink>
-              </li>
-            </ul>
-          </nav>
-          <button className={classes.btn_burger__menu}>1 day discount!</button>
-        </div>
-      )}
+
+      <div
+        className={`${classes.central_container__nav} ${
+          burgerMenuOpen ? classes.active : ""
+        }`}
+      >
+        <button className={classes.btn}>1 day discount!</button>
+        <button onClick={handleClickToggle} className={classes.cross_btn}>
+          <div className={classes.cross}>
+            <span className={classes.span_bevelled__top}></span>
+            <span className={classes.span_bevelled__botton}></span>
+          </div>
+        </button>
+
+        <nav className={classes.nav_container}>
+          <ul>
+            <li>
+              <NavLink className={classes.nav_element__style}>
+                Main Page
+              </NavLink>
+            </li>
+          </ul>
+          <ul>
+            <li>
+              <NavLink className={classes.nav_element__style}>
+                Categories
+              </NavLink>
+            </li>
+          </ul>
+          <ul>
+            <li>
+              <NavLink className={classes.nav_element__style}>
+                All products
+              </NavLink>
+            </li>
+          </ul>
+          <ul>
+            <li>
+              <NavLink className={classes.nav_element__style}>
+                All sales
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+
+        <button className={classes.btn_burger__menu}>1 day discount!</button>
+      </div>
 
       <div className={classes.container_icon__menu}>
         <div className={classes.icon_container}>
@@ -71,10 +77,7 @@ export const HeaderNav = () => {
           <SvgBascket />
         </div>
 
-        <button
-          className={classes.menu_btn}
-          onClick={() => setBurgerMenuOpen(!burgerMenuOpen)}
-        >
+        <button className={classes.menu_btn} onClick={handleClickToggle}>
           <div className={classes.menu}>
             <span></span>
             <span></span>
