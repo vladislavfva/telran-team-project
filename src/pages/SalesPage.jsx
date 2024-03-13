778bb932be4aedf3e92c0b54c300ef3e5493dc3d
vladislavfva <vladislavfva@icloud.com>
2024-03-13 14:46:23 +0100

    Before sales page

--- src/pages/SalesPage.jsx
+++ src/pages/SalesPage.jsx
@@ -5,8 +5,24 @@ function SalesPage() {
   product.discont_price && product.discont_price < product.price;
 
   return ( <>
-    <p>Sales Page</p>
+    <h2>Discounted items</h2>
+    <div>
+      <div>
+        <p>Price</p>
+        <input type="text" placeholder="from"/> 
+        <input type="text" placeholder="to"/>
+      </div>
+      <div>
+      <p>Sorted</p>
+        <select name="sort" id="sort">
+          <option value="decrease">decrease</option>
+          <option value="increase">increase</option>
+        </select>
+      </div>
+    </div>
+    <div className='section'>
     {<ProductsList filterFunction={discountFilter}/>}
+    </div>
   </> );
 }
 
