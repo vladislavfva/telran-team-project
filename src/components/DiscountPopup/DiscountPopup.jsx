f0211d4fd441935d35c02059356ebd8f84b3cc87
vladislavfva <vladislavfva@icloud.com>
2024-04-05 11:47:26 +0200

    Day discount Popup

--- src/components/DiscountPopup/DiscountPopup.jsx
+++ src/components/DiscountPopup/DiscountPopup.jsx
@@ -1,32 +1,35 @@
+import { useDispatch } from 'react-redux';
+import classes from './DiscountPopup.module.css'
 import CloseIcon from "./DiscountPopupIcons";
+import { addToCart } from '../../store/slices/cartSlice';
 
-function DiscountPopup({product}) {
-  return ( <div>
-    <div>
-      <p>50% discount on product of the day!</p>
-      <div>
-        <img src={process.env.REACT_APP_BACKEND_BASE_URL + product.image} alt={product.title} />
-        <p>{product.title}</p>
-        {product.discont_price ? (
-            <>
-              <p>${product.discont_price}</p>
-              <p>${product.price}</p>
-              <div>
-                {product.discont_price
-                ? '-' + Math.ceil(((product.price - product.discont_price) * 100) / product.price) + '%'
-                : ''}
+function DiscountPopup({product, isActive, onClose}) {
+  const dispatch = useDispatch();
+  if (!product) return null;
+
+
+  if(isActive) {
+    return ( <div className={classes.popup}>
+      <div className={classes.popup_card}>
+        <p className={classes.title}>50% discount on product of the day!</p>
+        <div className={classes.info}>
+          <img className={classes.popup_img} src={process.env.REACT_APP_BACKEND_BASE_URL + product.image} alt={product.title} />
+          <div className={classes.discount}>-50%</div>
+          <div className={classes.text_info}>
+            <p className={classes.product_title}>{product.title}</p>
+            <p className={classes.current_price}>${product.discont_price}</p>
+            <p className={classes.price_before}>${product.price}</p>
+          </div>
         </div>
-            </>
-          ) : (
-            <p>${product.price}</p>
-        )}
+        <button className={classes.button} onClick={() => dispatch(addToCart({product}))}>Add to cart</button>
+        <button className={classes.close} onClick={onClose}>
+          <CloseIcon />
+        </button>
       </div>
-      <button>Add to cart</button>
-      <button>
-        <CloseIcon />
-      </button>
-    </div>
-  </div> );
+    </div> );
+  } else {
+    return null;
+  }
 }
 
 export default DiscountPopup;
